af46635488e12fdb8f44d7f45737de79bffdd9e5
vantezzen <properly@protonmail.com>
2018-11-30 21:51:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<p align="center">
+    <img src="icons/icon-500.png" height="300"><br />
+    <a href="https://addons.mozilla.org/en-US/firefox/addon/dontbugme/">
+        <img src="icons/firefox.png" alt="Availible on Firefox Add-Ons" width="150">
+    </a>
+</p>
+
+# DontBugMe
+DontBugMe is a web browser extension that simplifies the usage of login credentials from [BugMeNot](http://bugmenot.com/).
+
+## Screenshot
+![Screenshot](screenshot.png)
+
+## Installation
+DontBugMe is availible through the [Firefox Add-On Platform](https://addons.mozilla.org/en-US/firefox/addon/dontbugme/).
+You can also install this extension by downloading the source from GitHub and loading the unpacked extension through "about:debugging" as a "Temporary Add-On".
+
+DontBugMe is currently not availible for Chrome.
+
+## Contributing
+Please fork this repository and create a new pull request to contribute to it.
+
+If you notice any errors, please create a new issue on GitHub.
+
+## Disclaimer
+This extension is not affiliated with BugMeNot. It is developed completely independentely.
+
---
+++ popup/js/popup.js
@@ -0,0 +1,130 @@
+/**
+ * DontBugMe Browser Extension.
+ * Easily insert credentials from BugMeNot.com into any page.
+ * This extension is not affiliated to BugMeNot.com in any way.
+ * 
+ * @copyright   Copyright vantezzen (https://github.com/vantezzen)
+ * @link        https://github.com/vantezzen/dontbugme
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+// Domain of the current tab
+let domain;
+
+// Current tab instance
+let tab;
+
+// If autosubmit is enabled, check the checkbox
+if (localStorage.getItem('autosubmit') == 'yes') {
+    $('#autosubmit').prop('checked', true);
+}
+
+// Update localStorage variable when autosubmit checkbox gets clicked
+$('#autosubmit').click(function() {
+    if ($('#autosubmit').prop('checked')) {
+        localStorage.setItem('autosubmit', 'yes');
+    } else {
+        localStorage.setItem('autosubmit', 'no');
+    }
+});
+
+// Get the domain of the current tab
+function getTabDomain(tabs) {
+    tab = tabs[0];
+
+    // Get the full domain name (e.g. sub.domain.example.com)
+    let fullUrl = tab.url.match(/:\/\/(.[^/]+)/)[1];
+
+    // Get the main domain (e.g. example.com)
+    let split = fullUrl.split('.');
+    domain = split[split.length - 2] + '.' + split[split.length - 1];
+
+    console.debug("Current domain is " + domain);
+
+    // Get logins for the current domain
+    getLogins();
+}
+
+// Log errors to the console
+function onError(err){
+    console.error(err);
+}
+
+// Query for current open tab
+browser.tabs.query({currentWindow: true, active: true}).then(getTabDomain, onError);
+
+// Get logins for current tab
+function getLogins() {
+    // Reset logins list on page
+    $('.logins').text('Searching for logins...');
+
+    // Make GET request to bugmenot.com for current domain
+    jQuery.get('http://bugmenot.com/view/' + domain).done(function(data) {
+        // Turn response into jQuery object
+        let page = $(data).find('#content');
+
+        // Check if there are accounts
+        if (page.find('.account').length == 0) {
+            $('.logins').html('It looks like there are no availible logins for this page.');
+        } else {
+            // Clear logins list
+            $('.logins').html('');
+        }
+
+        // Loop through accounts on bugmenot page
+        page.find('.account').each(function(el) {
+            // Get username and password from page
+            let user = $(this).find('kbd').first().text();
+            let pass = $(this).find('kbd').eq(1).text();
+
+            // Create new element for logins list
+            let element = $(`
+                <li class="list-group-item login">
+                    Username: ` + user + `<br />
+                    Password: ` + pass + `
+                </li>
+            `);
+
+            // Add click listener to element
+            element.click(function() {
+                // Insert username and password into tab
+                // Try to find username input
+                executeInTab("document.querySelector('input[name=email]').value = '" + user + "';");
+                executeInTab("document.querySelector('input[name=mail]').value = '" + user + "';");
+                executeInTab("document.querySelector('input[name=user]').value = '" + user + "';");
+                executeInTab("document.querySelector('input[name=username]').value = '" + user + "';");
+                executeInTab("document.querySelector('input[id=email]').value = '" + user + "';");
+                executeInTab("document.querySelector('input[id=mail]').value = '" + user + "';");
+                executeInTab("document.querySelector('input[id=user]').value = '" + user + "';");
+                executeInTab("document.querySelector('input[id=username]').value = '" + user + "';");
+                executeInTab("document.querySelector('input[type=text]').value = '" + user + "';");
+
+                executeInTab("document.querySelector('input[type=password]').value = '" + pass + "';");
+
+                // Submit if autosubmit is on
+                if ($('#autosubmit').prop('checked')) {
+                    // Get form of username box and submit it
+                    executeInTab("document.querySelector('input[type=password]').form.submit()");
+
+                    // Close window as saved page is invalid from now
+                    window.close();
+                }
+            });
+
+            // Append new element to logins list
+            $('.logins').append(element);
+
+            // Log username and password for debugging purposes
+            console.log(user, pass);
+        });
+    }).fail(function(error) {
+        console.log(error);
+    });
+}
+
+// Execute JavaScript code in current open tab
+function executeInTab(code) {
+    browser.tabs.executeScript({
+        code
+    });
+}
