c68fd3636edca2b7f369278548b12d27abff865a
Bennett <hello@vantezzen.io>
2022-06-12 15:17:01 +0200

    Prepare submissions

--- src/popup/typings.d.ts
+++ src/popup/typings.d.ts
@@ -1,3 +1,6 @@
 declare interface Window {
-  plausible: (name : string, options?: { callback?: () => void, props?: { [key: string]: any } }) => void
-};
+  plausible: (
+    name: string,
+    options?: { callback?: () => void; props?: { [key: string]: any } }
+  ) => void;
+}
